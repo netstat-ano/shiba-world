1db7ca02dca327888de464627f088c70bf852ec0
netstat-ano <bartosz147258@gmail.com>
2022-01-05 20:56:28 +0100

    Changing change room component

--- src/components/ChangeRoom/ChangeRoom.js
+++ src/components/ChangeRoom/ChangeRoom.js
@@ -4,37 +4,65 @@ import { useContext, useState } from 'react';
 const ChangeRoom = () => {
     const roomCtx = useContext(PlaceContext);
     const [roomIndex, setRoomIndex] = useState(2);
-    const onChangeLeftHandler = () => {
-        roomCtx.setRoom(roomCtx.availableRooms[roomIndex - 1]);
-        if (roomCtx.currentRoom === undefined) {
-            roomCtx.setRoom('garden');
-            setRoomIndex(roomCtx.availableRooms.length - 1);
-        } else {
-            setRoomIndex((prevState) => prevState - 1);
-        }
-        if (roomIndex < 0) {
-            setRoomIndex(roomCtx.availableRooms.length);
+    const onChangeDirectionHandler = (
+        type,
+        array,
+        startingPosition,
+        setStartingPosition,
+        setValue
+    ) => {
+        if (type === 'left') {
+            if (startingPosition - 1 <= 0) {
+                setStartingPosition(array.length);
+            } else if (startingPosition - 1 >= array.length) {
+                setStartingPosition(array.length);
+            } else {
+                setStartingPosition(startingPosition - 1);
+            }
+            setValue(array[startingPosition - 1]);
+        } else if (type === 'right') {
+            if (startingPosition + 1 >= array.length - 1) {
+                setStartingPosition(-1); //jesli starting position + 1 jest wieksze od dlugosci tablicy - 1
+            } else if (startingPosition + 1 < 0) {
+                setStartingPosition(-1); //jesli starting postion + 1 jest mniejsze od zera
+            } else {
+                setStartingPosition(startingPosition + 1); //zawsze
+            }
+            setValue(array[startingPosition + 1]);
         }
     };
-    const onChangeRightHandler = () => {
-        roomCtx.setRoom(roomCtx.availableRooms[roomIndex + 1]);
-        if (roomCtx.currentRoom === undefined) {
-            roomCtx.setRoom('living-room');
-            setRoomIndex((prevState) => 0);
-        } else {
-            setRoomIndex((prevState) => prevState + 1);
-        }
-        if (roomIndex > 4) {
-            setRoomIndex(0);
-        }
-    };
+
     console.log(roomIndex);
     console.log(roomCtx.currentRoom);
     return (
         <div className={styles.center}>
-            <button onClick={onChangeLeftHandler}>Arrow left</button>
+            <button
+                onClick={() => {
+                    onChangeDirectionHandler(
+                        'left',
+                        roomCtx.availableRooms,
+                        roomIndex,
+                        setRoomIndex,
+                        roomCtx.setRoom
+                    );
+                }}
+            >
+                Arrow left
+            </button>
             <label>{roomCtx.currentRoom}</label>
-            <button onClick={onChangeRightHandler}>Arrow right</button>
+            <button
+                onClick={() => {
+                    onChangeDirectionHandler(
+                        'right',
+                        roomCtx.availableRooms,
+                        roomIndex,
+                        setRoomIndex,
+                        roomCtx.setRoom
+                    );
+                }}
+            >
+                Arrow right
+            </button>
         </div>
     );
 };
